eacabdc3ef3db720dc4e1919c756bcdc7714b357
minji <flowingmj@gmail.com>
2018-04-05 18:25:54 +0900

    [Modified] #29 item registration

--- choiminji/vending_machine/public/js/vm.js
+++ choiminji/vending_machine/public/js/vm.js
@@ -1,13 +1,29 @@
 const Items = (() => {
 	const ITEM_LIST = new Map();
-	const PRICE = symbol("price");
-	const CONDITION = symbol("condition");
-	const COUNT = symbol("count");
-	const SHOW = symbol("show");
 
 	return class {
 		constructor () {
 			//
 		}
+		add (itemName, option) {
+			if ( !itemName ) throw "상품명 누락";
+			if ( typeof option !== "object") throw "상품 옵션 형식 오류";
+			if ( !option.PRICE ) throw "가격은 필수옵션!"
+			else if ( option.PRICE && isNaN(option.PRICE) ) throw "가격은 숫자만 입력하기";
+			if ( isNaN(option.COUNT) ) throw "재고량은 숫자만 입력하기";
+
+			const defaultOption = {
+				CONDITION : "ICE",
+				COUNT : 0,
+				SHOW : false
+			};
+
+			const itemOption = Object.assign(defaultOption, option);
+
+			if ( ITEM_LIST.get(itemName) ) throw "중복된 상품이 있습니다. 상품명을 다시 입력해주세요";
+			ITEM_LIST.set(itemName, itemOption);
+		}
 	}
-});
+})();
+
+const VM1 = new Items();
