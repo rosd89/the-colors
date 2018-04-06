32f4d0adaf10550052d66a2c16284dec552d6e60
minji <flowingmj@gmail.com>
2018-04-06 18:41:26 +0900

    [Modified] item/inventory 구분

--- choiminji/vending_machine/public/js/vm.js
+++ choiminji/vending_machine/public/js/vm.js
@@ -1,29 +1,50 @@
-const Items = (() => {
-	const ITEM_LIST = new Map();
+// 04.06 pm 5:07 ~ pm 6:30
+const CONDITION = ["ICE", "HOT"];
 
-	return class {
-		constructor () {
-			//
-		}
-		add (itemName, option) {
-			if ( !itemName ) throw "상품명 누락";
-			if ( typeof option !== "object") throw "상품 옵션 형식 오류";
-			if ( !option.PRICE ) throw "가격은 필수옵션!"
-			else if ( option.PRICE && isNaN(option.PRICE) ) throw "가격은 숫자만 입력하기";
-			if ( isNaN(option.COUNT) ) throw "재고량은 숫자만 입력하기";
-
-			const defaultOption = {
-				CONDITION : "ICE",
-				COUNT : 0,
-				SHOW : false
-			};
-
-			const itemOption = Object.assign(defaultOption, option);
-
-			if ( ITEM_LIST.get(itemName) ) throw "중복된 상품이 있습니다. 상품명을 다시 입력해주세요";
-			ITEM_LIST.set(itemName, itemOption);
-		}
+class Item {
+	constructor(name, price, condition, count, display) {
+		this.name = name;
+		this.price = price;
+		this.condition = condition;
+		this.count = count;
+		this.display = display;
 	}
-})();
 
-const VM1 = new Items();
+	getPrice () { return this.price; }
+	getCondition () { return this.condition; }
+	getCount () { return this.count; }
+	getDisplay () { return this.display; }
+};
+
+class Inventory {
+	constructor () {
+		this.ITEM_LIST = [];
+	}
+
+	add (name, price, condition = CONDITION[0], count = 0, display = false) {
+		if ( !name ) vmError("상품명 누락");
+		if ( !price ) vmError("가격은 필수옵션!");
+		else if ( price && isNaN(price) ) vmError("가격은 숫자만 입력하기");
+		if ( isNaN(count) ) vmError("재고량은 숫자만 입력하기");
+
+		const item = new Item(name, price, condition, count, display);
+		console.log(item);
+
+		this.ITEM_LIST.forEach(v => {
+			if ( v.name === name ) vmError("중복된 상품이 있습니다. 상품명을 다시 입력해주세요");
+		})
+
+		this.ITEM_LIST.push(item);
+		console.log(this.ITEM_LIST);
+	}
+}
+
+const vmError = msg => {
+	// try {
+	// 	throw new Error(msg);
+	// } catch (e) {
+	// 	console.log("Vending Machine Error : "+e);
+	// }
+}
+
+const VM1 = new Inventory();
