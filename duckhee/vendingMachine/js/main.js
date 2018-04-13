8b782d1f041ad8caa0007f8a96d6844901f4ab05
LEEDUCKHEE <shldhee@yahoo.co.kr>
2018-04-13 10:55:54 +0900

    [Create] function removeDisplayedSpace

--- duckhee/vendingMachine/js/main.js
+++ duckhee/vendingMachine/js/main.js
@@ -155,6 +155,15 @@ var VendingMachine = (function(){
     }
   }
 
+  // 진열 공간에 상품 제거
+  VendingMachine.prototype.removeDisplayedSpace = function (id) {
+    var targetIndex = this.displayedSpace.findIndex(function (item) {
+      return id === item.id;
+    });
+
+    this.displayedSpace.splice(targetIndex, 1);
+  }
+
   // 진열 공간 확인
   VendingMachine.prototype.showDisplayedSpace = function (product) {
     console.log(this.displayedSpace);
