4293aede4c29b21782d33f801b7f36ae86941e4c
Jeewhan <distinct19@gmail.com>
2018-04-15 03:45:34 +0900

    [Create] reducer

---
+++ jeewhan/vending_machine/FE/reducer.js
@@ -0,0 +1,78 @@
+const generator = f => immer.default(vm.store, f);
+
+const fetcher = async newStore => {
+  return await fetch("http://localhost:3000/", {
+    body: JSON.stringify(newStore),
+    method: "POST"
+  }).then(res => res.json());
+};
+
+const money = payload =>
+  generator(({ session }) => {
+    session.change += payload;
+  });
+
+const number = payload =>
+  generator(({ session }) => {
+    if (!session.select) session.select = payload;
+    else if (session.select.length === 2)
+      session.select = session.select[1] + payload;
+    else session.select += payload;
+  });
+
+const change = payload =>
+  generator(({ session }) => {
+    session.change = 0;
+  });
+
+const selectAndProduct = payload =>
+  generator(({ products, session, log }) => {
+    const { price } = products[payload];
+    const today = dateFns.format(new Date(), "YYMMDD");
+    products[payload].stock -= 1;
+    session.change -= price;
+    log.daily[today] ? (log.daily[today] += price) : (log.daily[today] = price);
+    log.products[payload].revenue += price;
+  });
+
+const add = payload =>
+  generator(({ products, log }) => {
+    products.push(payload);
+    log.products.push({ name: payload.name, revenue: 0 });
+  });
+
+const remove = payload =>
+  generator(({ products, log }) => {
+    products.splice(payload, 1);
+    log.products.splice(payload, 1);
+  });
+
+const power = payload =>
+  generator(({ power }) => {
+    power = payload;
+  });
+
+const fill = payload =>
+  generator(({ products }) => {
+    products[payload.id].stock += payload.stock;
+  });
+
+const updater = {
+  money,
+  number,
+  change,
+  select: selectAndProduct,
+  product: selectAndProduct,
+  add,
+  remove,
+  power,
+  fill
+};
+
+vm.reducer = ({ type, payload }) => {
+  const newStore = updater[type](payload);
+
+  return fetcher(newStore)
+    ? (vm.store = newStore)
+    : (console.error(newStore), {});
+};
