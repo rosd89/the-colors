06bcc91443d16380329fd373ebc34fe53c226f4b
LEEDUCKHEE <shldhee@yahoo.co.kr>
2018-05-09 19:42:50 +0900

    [Modify] #91 edit after code review

--- duckhee/carousel/src/main.js
+++ duckhee/carousel/src/main.js
@@ -1,39 +1,70 @@
 var obj = {
-  width: '640px',
-  height: '400px',
-  margin: '0 auto',
+  width: 320,
+  height: 160,
+  mode: 'center',
   images: [
-    'http://placehold.it/640X400/555',
-    'http://placehold.it/640X400/666',
+    'http://placehold.it/320X160/555',
+    'http://placehold.it/320X160/666',
   ],
 };
 
-var root = document.getElementById('root'),
-  slideWrap = createAddedClassTag('div', 'slideWrap'),
-  slide = createAddedClassTag('div', 'slide'),
-  slideList = createAddedClassTag('ul', 'slideList');
+var root = document.getElementById('root');
+var slideWrap = createAddedClassTag('div', 'slideWrap');
+var slide = createAddedClassTag('div', 'slide');
+var slideList = createAddedClassTag('ul', 'slideList');
 
 function Carousel(obj) {
-  this.width = obj.width || '800px';
-  this.height = obj.height || '600px';
-  this.margin = obj.margin || '0 auto';
-  this.images = obj.images || 'http://placehold.it/640X400/000';
+  if (obj.constructor !== Object) {
+    console.warn('invalid obj');
+  }
+  this.width = obj.width || 800;
+  this.height = obj.height || 600;
+  this.mode = obj.mode || 'center';
+  this.images = obj.images || 'http://placehold.it/320X160/000';
 
   this.init();
 }
 
 Carousel.prototype = {
   init: function() {
-    var i,
-      css = {
-        width: this.width,
-        height: this.height,
-        margin: this.margin,
-        backgroundColor: 'red',
-      };
+    var imageIndex;
+    var top;
+    var left;
+    var right;
+    var marginLeft;
 
-    for (i in css) {
-      slideWrap.style[i] = css[i];
+    switch (this.mode) {
+      case 'left':
+        left = 0;
+        right = 'auto';
+        break;
+
+      case 'center':
+        left = '50%';
+        marginLeft = -parseInt(this.width) / 2;
+        break;
+
+      case 'right':
+        right = 0;
+        left = 'auto';
+        break;
+
+      default:
+        break;
+    }
+
+    var imageStyle = {
+      width: this.width + 'px',
+      height: this.height + 'px',
+      position: 'absolute',
+      left: left,
+      right: right,
+      marginLeft: marginLeft ? marginLeft + 'px' : 0,
+      backgroundColor: 'red',
+    };
+
+    for (imageIndex in imageStyle) {
+      slideWrap.style[imageIndex] = imageStyle[imageIndex];
     }
 
     root.appendChild(slideWrap);
@@ -43,8 +74,8 @@ Carousel.prototype = {
   render: function() {
     var docFragment = document.createDocumentFragment();
     this.images.forEach(function(e) {
-      var li = createAddedClassTag('li', 'slideItem'),
-        image = createAddedClassTag('img', 'slideImg');
+      var li = createAddedClassTag('li', 'slideItem');
+      var image = createAddedClassTag('img', 'slideImg');
       image.src = e;
       li.appendChild(image);
       docFragment.appendChild(li);
