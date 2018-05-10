59a52fb060fcdedceb5f25a25ccb29e7813a9933
minji <flowingmj@gmail.com>
2018-05-10 19:03:55 +0900

    [Modify] set basic options

--- choiminji/carousel/public/js/crousel.js
+++ choiminji/carousel/public/js/crousel.js
@@ -4,38 +4,65 @@ var SlideElement = function(option) {
 
 var Slide = (function () {
   var SLIDE_LIST = [];
-
   var DEFAULT_OPTION = {
+    width : 1000,
+    height : 600,
     autoPlay : false,
     direction : 'default',  // default, left, right
     speed : 300,
     arrow : true,
     dot : true
   };
-  Object.freeze(DEFAULT_OPTION);
+  Object.freeze(DEFAULT_OPTION);  
+  
+  return function(userOption){
+    var optValid = this.optionValidation(userOption);
+    if (optValid) {
+      var option = Object.assign({}, DEFAULT_OPTION, userOption);
+      this.option = option;
+      this.init();
+    } else {
+      warn('option 값을 확인하세요.');
+      return;
+    };
+  }
+})();
 
-  var optionValidation = function(option) {
+Slide.prototype = {
+  optionValidation : function(option) {
     var optValid = validation.isObject(option);
-    var [idNameValid, slideListValid, autoPlayValid, directionValid, speedValid, arrowValid, dotValid] = [true,true,true,true,true,true,true];
+    var idNameValid = true;
+    var slideListValid = true;
+    var widthValid = true;
+    var heightValid = true;
+    var autoPlayValid = true;
+    var directionValid = true;
+    var speedValid = true;
+    var arrowValid = true;
+    var dotValid = true;
     
     if (option.idName) idNameValid = validation.isString(option.idName);
     if (option.slideList) slideListValid = validation.isArr(option.slideList);
+    if (option.width) widthValid = validation.isNumber(option.width);
+    if (option.height) heightValid = validation.isNumber(option.height);
     if (option.autoPlay) autoPlayValid = validation.isBoolean(option.autoPlay);
     if (option.direction) directionValid = validation.isString(option.direction);
     if (option.speed) speedValid = validation.isNumber(option.speed);
     if (option.arrow) arrowValid = validation.isBoolean(option.arrow);
     if (option.dot) dotValid = validation.isBoolean(option.dot);
 
-    return idNameValid && slideListValid && autoPlayValid && directionValid && speedValid && arrowValid && dotValid;
-  }
+    return idNameValid && slideListValid && widthValid && heightValid && autoPlayValid && directionValid && speedValid && arrowValid && dotValid;
+  },
+  init : function() {
+    this.currentIdx = 0;
+    this.slideLength = this.option.slideLength.length;
+    this.direction = this.option.direction;
 
-  return function(userOption){
-    var optValid = optionValidation(userOption);
-    if (optValid) {
-     var option = Object.assign({}, DEFAULT_OPTION, userOption);
-    };
+    if (this.option.dot) this.createDot(this.slideLength);
+    if (this.option.arrow) this.createArrow();
   }
-})();
+}
+
 
 var validation = (function() {
   var isObject = function(obj) {
@@ -51,7 +78,6 @@ var validation = (function() {
   }
   
   var isBoolean = function(boolean) {
-    console.log("--------"+boolean);
     return boolean !== null && typeof boolean === 'boolean';
   }
 
@@ -67,3 +93,7 @@ var validation = (function() {
     isNumber : isNumber
   }
 })();
+
+var warn = function(msg) {
+  console.log(msg);
+};
