3fddddb3899f75ce432f125356dfa4609f0910d0
minji <flowingmj@gmail.com>
2018-06-14 19:08:29 +0900

    [Modify] edit keyevent

--- choiminji/calculator/public/js/calculator.js
+++ choiminji/calculator/public/js/calculator.js
@@ -13,33 +13,10 @@ var Calculator = (function() {
     multiply : "*",
     division : "/"
   };
-
-  var KEY_CODE = {
-    number : {
-      0 : 48,
-      1 : 49,
-      2 : 50,
-      3 : 51,
-      4 : 52,
-      5 : 53,
-      6 : 54,
-      7 : 55,
-      8 : 56,
-      9 : 57
-    },
-    operator : {
-      "+" : 43,
-      "-" : 45,
-      "*" : 42,
-      "/" : 47
-    },
-    equal : {
-      "=" : 61, 
-      "=" : 13
-    },
-    ac : {
-      "AC" : 27
-    }
+  var ETC_BUTTON = {
+    esc : "Escape",
+    enter : "Enter",
+    backspace : "Backspace"
   }
 
   var temporaryExpression = "";
@@ -50,30 +27,72 @@ var Calculator = (function() {
     var clickedButton = container.querySelectorAll(".button ul li");
     
     clickedButton.forEach(function(v, i) {
-      clickedButton[i].addEventListener("click", function(e) {
-        var target = e.target;
+      clickedButton[i].addEventListener("click", function(event) {
+        var target = event.target;
         var key = target.getAttribute("data-key");
+        clickedKeyType = checkKeyType(key);
 
-        clickButton(key)
+        clickButton(key, clickedKeyType)
       })
     })
   };
 
   var addKeyEvent = function() {
+    var inputBox = container.querySelector(".calculation_curr input");
     
+    inputBox.onkeydown = function(event) {
+      var keyValue = event.key;
+      clickedKeyType = checkKeyType(keyValue);
+
+      if ( clickedKeyType ) clickButton(keyValue, clickedKeyType);
+
+      event.preventDefault();
+    };
   }
 
-  var clickButton = function(key) {
-    clickedKeyType = checkKeyType(key);
-    console.log("keyType ---", clickedKeyType);
+  var checkKeyType = function(key) {
+    if (key === null) {
+      warn("빈값입니다.");
+      return false;
+    }
 
+    if ( key === KEY_TYPE.ac || key === ETC_BUTTON.esc ) {
+      return KEY_TYPE.ac;
+
+    } else if ( key === KEY_TYPE.equal || key === ETC_BUTTON.enter ) {
+      return KEY_TYPE.equal;
+
+    } else if ( key === ETC_BUTTON.backspace) {
+      return ETC_BUTTON.backspace;
+
+    } else if ( validation.isNumber(Number(key)) ) {
+      return KEY_TYPE.number;
+
+    } else if ( key === OPERATOR_BUTTON.plus || key === OPERATOR_BUTTON.minus || key === OPERATOR_BUTTON.multiply || key === OPERATOR_BUTTON.division ) {
+      return KEY_TYPE.operator;
+
+    } else {
+      return false;
+    };
+  };
+  
+  var clickButton = function(key, clickedKeyType) {
     switch (clickedKeyType){
       case KEY_TYPE.ac :
         clearAll();
         printDisplay();
         break;
       case KEY_TYPE.equal :
-        getResult();
+        var invalid = checkDuplicate();
+        console.log("invalid equal ", invalid)
+        if (invalid) {
+          addExpression();
+          getResult();
+        }
+        break;
+      case ETC_BUTTON.backspace :
+        clearOnce();
+        printDisplay();
         break;
       case KEY_TYPE.number :
         var invalid = checkDuplicate();
@@ -91,22 +110,6 @@ var Calculator = (function() {
     }
   };
 
-  var checkKeyType = function(key) {
-    if (key === null) {
-      warn("빈값입니다.");
-      return false;
-    }
-
-    if ( key === KEY_TYPE.ac ) {
-      return KEY_TYPE.ac;
-    } else if ( key === KEY_TYPE.equal ) {
-      return KEY_TYPE.equal;
-    } else if ( validation.isNumber(Number(key)) ) {
-      return KEY_TYPE.number;
-    } else {
-      return KEY_TYPE.operator;
-    }
-  };
 
   var inputNumber = function(key) {
     temporaryExpression += Number(key);
@@ -119,12 +122,13 @@ var Calculator = (function() {
   };
 
   var checkDuplicate = function() {
+    console.log("checkDuplicate", clickedKeyType, temporaryExpression);
     if (clickedKeyType === KEY_TYPE.operator && temporaryExpression === "") {
       return false;
     } else {
       return true;
     }
-  }
+  };
 
   var printDisplay = function() {
     var expression = EXPRESSION.slice();
@@ -134,10 +138,9 @@ var Calculator = (function() {
     var expressionCurrHTML = container.querySelector(".calculation_curr input");
 
     expressionHTML.innerText = expression.join(" ");
-    // expressionCurrHTML.value = "";
     expressionCurrHTML.value = expressionCurr;
     expressionCurrHTML.focus();
-  }
+  };
   
 
   var clearAll = function() {
@@ -145,7 +148,13 @@ var Calculator = (function() {
     temporaryExpression = "";
   };  
 
-  var getResult = function() {  };
+  var clearOnce = function() {
+    temporaryExpression = temporaryExpression.slice(0, -1);
+  }
+
+  var getResult = function() {
+    infixToPostfix(EXPRESSION);
+  };
 
   return function(target){
     container = document.querySelector(target);
@@ -155,6 +164,10 @@ var Calculator = (function() {
   };
 })();
 
+var infixToPostfix = function(exp) {
+  var expression = exp;
+  console.log("test", expression);
+}
 var warn = function(msg) {
   var msgBox = document.querySelector("#msg");
   msgBox.innerText = msg;
