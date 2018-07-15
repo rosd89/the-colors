66a555832c6f81e39b680f676a6b9382947071a0
mmmindy <flowingmj@gmail.com>
2018-07-15 23:45:48 +0900

    [Modify] add item

--- choiminji/vending_machine_2/vm_project/src/App.js
+++ choiminji/vending_machine_2/vm_project/src/App.js
@@ -2,10 +2,25 @@ import React, { Component } from 'react';
 import VMForm from './components/VMForm';
 
 class App extends Component {
+  idx = 0;
+  state = {
+    itemList : []
+  }
+  _handleCreate = data => {
+    const { itemList } = this.state;
+    console.log(this,itemList, this.state);
+    this.setState({
+      itemList : itemList.concat({
+        ...data,
+        idx : this.idx++
+      })
+    })
+    console.log(this.state.itemList)
+  }
   render() {
     return (
       <div>
-        <VMForm />
+        <VMForm onCreate={this._handleCreate}/>
       </div>
     );
   }
--- choiminji/vending_machine_2/vm_project/src/components/VMForm.js
+++ choiminji/vending_machine_2/vm_project/src/components/VMForm.js
@@ -1,15 +1,44 @@
 import React, { Component } from 'react';
 
 class VMForm extends Component {
+  state = {
+    name : '',
+    price :'',
+    count : ''
+  }
+  _handleChange = e => {
+    console.log(e.target.name);
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+  _handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onCreate(this.state);
+    
+    this.setState({
+      name : '',
+      price : '',
+      count : ''
+    })
+  }
   render() {
     return (
-      <fragment>
-        <form>
-          <label>음료명 <input name='name'/></label>
-          <label>가격 <input name='name'/></label>
-          <label>재고 <input name='count'/></label>
+      <div>
+        <form onSubmit={this._handleSubmit}>
+          <label>음료명 
+            <input name='name' onChange={this._handleChange} value={this.state.name} />
+          </label>
+          <label>가격 
+            <input name='price' onChange={this._handleChange} value={this.state.price} />
+          </label>
+          <label>재고 
+            <input name='count' onChange={this._handleChange} value={this.state.count} />
+          </label>
+          <button type='submit'>등록하기!</button>
         </form>
-      </fragment>
+      </div>
     );
   }
 }
